8a3ba99560a83b1998809269e563419c670c57f2
donnwilzy <ask4godswill@gmail.com>
2023-01-07 14:40:38 +0100

    handle-bars templated for views

--- app.js
+++ app.js
@@ -1,14 +1,56 @@
 const express = require('express');
 const dotevn = require('dotenv');
-const connectDB = require('./config/db');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+
+//db linking
+/*
+const connectDB = require('./config/db') || mongoose.connect('mongodb://127.0.0.1:27017/member', {useNewUrlParser:true})
+const db = mongoose.connection;
+db.on('err', ()=>{
+    console.log('error in connection')
+})
+db.once('open',()=>{
+    console.log('connected')
+})
+*/
 
 
 //load config
 dotevn.config({ path: './config/config.env'})
 
-connectDB()
+//connectDB()
 const app = express();
 
+//logging 
+if (process.env.NODE_ENV === 'developmet'){
+    app.use(morgan('dev'))
+}
+
+//handlebars
+app.engine(
+    "hbs",
+    exphbs.engine({
+      extname: ".hbs",
+      defaultLayout: "main",
+      layoutsDir: "views/layout"
+    })
+  );
+  app.set('view engine', '.hbs');
+/*
+app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs'}));
+
+//  app.set('views', 'views'); 
+*/
+
+
+//port
 const PORT = process.env.PORT || 3000
+
+//Route
+app.use('/', require('./routes/index'))
+
+
 
 app.listen(PORT,()=>console.log(`PORT ${PORT} is active || 127.00.1/${PORT}`))
---
+++ routes/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+
+//loggin paage
+router.get('/', (req, res)=>{
+    res.render('login');
+})
+
+
+//register paage
+//route GET/ 
+router.get('/register', (req, res)=>{
+    res.render('register.hbs');
+})
+
+//dashboad paage
+//route GET/ 
+router.get('/dashboard', (req, res)=>{
+    res.render('dashboard.hbs');
+})
+
+
+module.exports = router;
